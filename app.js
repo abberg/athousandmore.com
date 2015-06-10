d89a3e8183620d5840a097d2789b1033d7ca05b7
abberg <andrewbberg@gmail.com>
2015-06-10 10:57:17 -0400

    bug fixes on date sort

--- app.js
+++ app.js
@@ -13,20 +13,24 @@ app.get('/', function (req, res) {
 	var folderList = fs.readdirSync(__dirname + '/public/sketches').filter(function(folder){ return !/^\./.test(folder); });
 	
 	folderList.sort(function(a,b){
+	
 		var aArr = a.split('_'),
 			bArr = b.split('_'),
 			c,
-			d,
+			d;
 		
-		aArr[0].split('-');
-		aArr[1].split('-');
-		bArr[0].split('-');
-		bArr[1].split('-');
+		aArr[0] = aArr[0].split('-');
+		aArr[1] = aArr[1].split('-');
+		bArr[0] = bArr[0].split('-');
+		bArr[1] = bArr[1].split('-');
 
-		c = new Date(aArr[0][2], aArr[0][0], aArr[0][1], aArr[1][0], aArr[1][1], aArr[1][2]);
-		d = new Date(bArr[0][2], bArr[0][0], bArr[0][1], bArr[1][0], bArr[1][1], bArr[1][2]));
+		c = new Date(aArr[0][2], aArr[0][0] - 1, aArr[0][1], aArr[1][0], aArr[1][1], aArr[1][2]);
+		d = new Date(bArr[0][2], bArr[0][0] - 1, bArr[0][1], bArr[1][0], bArr[1][1], bArr[1][2]);
 
-		return c-d;
+		if (c > d) return -1;
+  		if (c < d) return 1;
+  		return 0;
+	
 	});  
 	
 	res.render('index', { sketches: folderList });
